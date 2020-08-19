00c4698952b80cfd55257b6c37d9dbdf08efdb70
Nu-dang <69572561+Nu-dang@users.noreply.github.com>
2020-08-19 18:26:14 +0700

    Add files via upload

---
+++ WIFIMASTER/WIFIMASTER.ino
@@ -0,0 +1,315 @@
+/**
+   ESPNOW - Basic communication - Master
+   Date: 26th September 2017
+   Author: Arvind Ravulavaru <https://github.com/arvindr21>
+   Purpose: ESPNow Communication between a Master ESP32 and a Slave ESP32
+   Description: This sketch consists of the code for the Master module.
+   Resources: (A bit outdated)
+   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
+   b. http://www.esploradores.com/practica-6-conexion-esp-now/
+
+   << This Device Master >>
+
+   Flow: Master
+   Step 1 : ESPNow Init on Master and set it in STA mode
+   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
+   Step 3 : Once found, add Slave as peer
+   Step 4 : Register for send callback
+   Step 5 : Start Transmitting data from Master to Slave
+
+   Flow: Slave
+   Step 1 : ESPNow Init on Slave
+   Step 2 : Update the SSID of Slave with a prefix of `slave`
+   Step 3 : Set Slave in AP mode
+   Step 4 : Register for receive callback and wait for data
+   Step 5 : Once data arrives, print it in the serial monitor
+
+   Note: Master and Slave have been defined to easily understand the setup.
+         Based on the ESPNOW API, there is no concept of Master and Slave.
+         Any devices can act as master or salve.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <ADXL345.h>
+#include "ITG3200.h"
+#include "MovingAverageFilter.h"
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
+ITG3200 gyro;
+MovingAverageFilter movingAverageFilter(20);    
+int x,y,z;
+float xg,yg,zg;
+float soh;
+float tilt;
+float angle;
+String dir;
+float deltaGz;
+float gz_prev=0;
+float degx = 0;
+float anglex=0,angley=0,anglez=0;
+float out1=0,out2=0;
+uint8_t degree[8]  = {140,146,146,140,128,128,128,128};
+// Global copy of slave
+esp_now_peer_info_t slave;
+#define CHANNEL 3
+#define PRINTSCANRESULTS 0
+#define DELETEBEFOREPAIR 0
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// Scan for slaves in AP mode
+void ScanForSlave() {
+  int8_t scanResults = WiFi.scanNetworks();
+  // reset on each scan
+  bool slaveFound = 0;
+  memset(&slave, 0, sizeof(slave));
+
+  Serial.println("");
+  if (scanResults == 0) {
+    Serial.println("No WiFi devices in AP Mode found");
+  } else {
+    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
+    for (int i = 0; i < scanResults; ++i) {
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+      if (PRINTSCANRESULTS) {
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(SSID);
+        Serial.print(" (");
+        Serial.print(RSSI);
+        Serial.print(")");
+        Serial.println("");
+      }
+      //delay(10);
+      // Check if the current device starts with `Slave`
+      if (SSID.indexOf("Slave") == 0) {
+        // SSID of interest
+        Serial.println("Found a Slave.");
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+        // Get BSSID => Mac Address of the Slave
+        int mac[6];
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x%c",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int ii = 0; ii < 6; ++ii ) {
+            slave.peer_addr[ii] = (uint8_t) mac[ii];
+          }
+        }
+
+        slave.channel = CHANNEL; // pick a channel
+        slave.encrypt = 0; // no encryption
+
+        slaveFound = 1;
+        // we are planning to have only one slave in this example;
+        // Hence, break after we find one, to be a bit efficient
+        break;
+      }
+    }
+  }
+
+  if (slaveFound) {
+    Serial.println("Slave Found, processing..");
+  } else {
+    Serial.println("Slave Not Found, trying again.");
+  }
+
+  // clean up ram
+  WiFi.scanDelete();
+}
+
+// Check if the slave is already paired with the master.
+// If not, pair the slave with master
+bool manageSlave() {
+  if (slave.channel == CHANNEL) {
+    if (DELETEBEFOREPAIR) {
+      deletePeer();
+    }
+
+    Serial.print("Slave Status: ");
+    const esp_now_peer_info_t *peer = &slave;
+    const uint8_t *peer_addr = slave.peer_addr;
+    // check if the peer exists
+    bool exists = esp_now_is_peer_exist(peer_addr);
+    if ( exists) {
+      // Slave already paired.
+      Serial.println("Already Paired");
+      return true;
+    } else {
+      // Slave not paired, attempt pair
+      esp_err_t addStatus = esp_now_add_peer(peer);
+      if (addStatus == ESP_OK) {
+        // Pair success
+        Serial.println("Pair success");
+        return true;
+      } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
+        // How did we get so far!!
+        Serial.println("ESPNOW Not Init");
+        return false;
+      } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
+        Serial.println("Invalid Argument");
+        return false;
+      } else if (addStatus == ESP_ERR_ESPNOW_FULL) {
+        Serial.println("Peer list full");
+        return false;
+      } else if (addStatus == ESP_ERR_ESPNOW_NO_MEM) {
+        Serial.println("Out of memory");
+        return false;
+      } else if (addStatus == ESP_ERR_ESPNOW_EXIST) {
+        Serial.println("Peer Exists");
+        return true;
+      } else {
+        Serial.println("Not sure what happened");
+        return false;
+      }
+    }
+  } else {
+    // No slave found to process
+    Serial.println("No Slave found to process");
+    return false;
+  }
+}
+
+void deletePeer() {
+  const esp_now_peer_info_t *peer = &slave;
+  const uint8_t *peer_addr = slave.peer_addr;
+  esp_err_t delStatus = esp_now_del_peer(peer_addr);
+  Serial.print("Slave Delete Status: ");
+  if (delStatus == ESP_OK) {
+    // Delete success
+    Serial.println("Success");
+  } else if (delStatus == ESP_ERR_ESPNOW_NOT_INIT) {
+    // How did we get so far!!
+    Serial.println("ESPNOW Not Init");
+  } else if (delStatus == ESP_ERR_ESPNOW_ARG) {
+    Serial.println("Invalid Argument");
+  } else if (delStatus == ESP_ERR_ESPNOW_NOT_FOUND) {
+    Serial.println("Peer not found.");
+  } else {
+    Serial.println("Not sure what happened");
+  }
+}
+
+// send data
+
+ uint8_t data;
+  bool send_flag = 0;
+//SENSOR_DATA sd;
+char uart1_buf[300]; 
+void sendData() 
+{
+   float output = 0; 
+  //Boring accelerometer stuff   
+ adxl.readAccel(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  xg = x*0.0039;
+  yg = y*0.0039;
+  zg = z*0.0039;
+    if (zg<0.01) {zg=255.60;}
+    if (zg>200) {zg=zg-255.60;}
+soh = yg/zg;
+tilt = atan(soh)*57.296;
+
+    if (tilt < 0) {
+      tilt= -89.9/tilt*91;
+    } 
+    //Serial.println("Tilt:");Serial.println(angle);Serial.println(byte(0));Serial.println(dir);
+  
+int16_t x,y,z;
+    gyro.getXYZ(&x,&y,&z);
+    float wx,wy,wz;
+    gyro.getAngularVelocity(&wx,&wy,&wz);
+    anglex=wx*0.03;
+    angley=wy*0.03;
+    anglez=wy*0.03;
+    //anglex=abs(anglex);
+    out1= (degx+anglex);
+    degx = (wx/2000)*(degx + anglex)+(1-(wx/2000))*tilt;
+    output = movingAverageFilter.process(degx);
+    gz_prev = z;
+    //Serial.println(output);
+    data=output;
+  //for (int i = 0; i < SlaveCnt; i++) {
+    const uint8_t *peer_addr = slave.peer_addr;
+    if (send_flag == 0) { // print only for first slave
+      //Serial.print("Sending: ");
+      Serial.println(data);
+    }
+    esp_now_send(peer_addr, &data, sizeof(data));
+    //Serial.print("Send Status: ");
+    delay(30);
+}  
+
+// callback when data is sent from Master to Slave
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) 
+{
+  send_flag = 0;
+  /*char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");*/
+}
+
+void setup() {
+  Serial.begin(115200);
+  //Set device in STA mode to begin with
+  WiFi.mode(WIFI_STA);
+  Serial.println("ESPNow/Basic/Master Example");
+  // This is the mac address of the Master in Station Mode
+  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+  adxl.powerOn();
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+ // adxl.printAllRegister();
+  gyro.init();
+  gyro.zeroCalibrate(200,10);//sample 200 times to calibrate and it will take 200*10ms
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  ScanForSlave();
+}
+void loop() 
+{
+  // In the loop we scan for slave
+  
+  // If Slave is found, it would be populate in `slave` variable
+  // We will check if `slave` is defined and then we proceed further
+  if (slave.channel == CHANNEL) { // check if slave channel is defined
+    // `slave` is defined
+    // Add slave as peer if it has not been added already
+    bool isPaired = manageSlave();
+    if (isPaired) 
+    {
+      // pair success or already paired
+      // Send data to device
+      sendData();
+    } else {
+      // slave pair failed
+      Serial.println("Slave pair failed!");
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    }
+  }
+  else {
+    // No slave found to process
+  }
+//delay(10);
+  // wait for 3seconds to run the logic again
+}
---
+++ WIFISLAVE/WIFISLAVE.ino
@@ -0,0 +1,182 @@
+/**
+   ESPNOW - Basic communication - Slave
+   Date: 26th September 2017
+   Author: Arvind Ravulavaru <https://github.com/arvindr21>
+   Purpose: ESPNow Communication between a Master ESP32 and multiple ESP32 Slaves
+   Description: This sketch consists of the code for the Slave module.
+   Resources: (A bit outdated)
+   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
+   b. http://www.esploradores.com/practica-6-conexion-esp-now/
+   << This Device Slave >>
+   Flow: Master
+   Step 1 : ESPNow Init on Master and set it in STA mode
+   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
+   Step 3 : Once found, add Slave as peer
+   Step 4 : Register for send callback
+   Step 5 : Start Transmitting data from Master to Slave(s)
+   Flow: Slave
+   Step 1 : ESPNow Init on Slave
+   Step 2 : Update the SSID of Slave with a prefix of `slave`
+   Step 3 : Set Slave in AP mode
+   Step 4 : Register for receive callback and wait for data
+   Step 5 : Once data arrives, print it in the serial monitor
+   Note: Master and Slave have been defined to easily understand the setup.
+         Based on the ESPNOW API, there is no concept of Master and Slave.
+         Any devices can act as master or salve.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <ADXL345.h>
+#include "ITG3200.h"
+#include "MovingAverageFilter.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+
+#endif
+BluetoothSerial SerialBT;
+
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
+ITG3200 gyro;
+MovingAverageFilter movingAverageFilter(10000);    
+int x,y,z;
+float xg,yg,zg;
+float soh;
+float tilt;
+float angle;
+String dir;
+float deltaGz;
+float gz_prev=0;
+float degx = 0;
+float anglex=0,angley=0,anglez=0;
+float out1=0;
+int out2=0;
+
+uint8_t degree[8]  = {140,146,146,140,128,128,128,128};
+
+#define CHANNEL 1
+int H=0;
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// config AP SSID
+void configDeviceAP() {
+  String Prefix = "Slave:";
+  String Mac = WiFi.macAddress();
+  String SSID = Prefix + Mac;
+  String Password = "123456789";
+  bool result = WiFi.softAP(SSID.c_str(), Password.c_str(), CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  SerialBT.begin("ANGULIA001");
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial.println("ESPNow/Basic/Slave Example");
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  adxl.powerOn();
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  gyro.init();
+  gyro.zeroCalibrate(200,10);//sample 200 times to calibrate and it will take 200*10ms
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  //Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  //Serial.print("Last Packet Recv Data: ");
+   
+  H=*data;
+  
+}
+
+void loop() {
+  //Serial.println (H);
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    float output = 0; 
+  //Boring accelerometer stuff   
+  adxl.readAccel(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  xg = x*0.0039;
+  yg = y*0.0039;
+  zg = z*0.0039;
+    if (zg<0.01) {zg=255.60;}
+    if (zg>200) {zg=zg-255.60;}
+  soh = yg/zg;
+  tilt = atan(soh)*57.296;
+
+    if (tilt < 0) {
+      tilt= -89.9/tilt*91;
+    } 
+    //Serial.println("Tilt:");Serial.println(angle);Serial.println(byte(0));Serial.println(dir);
+  
+int16_t x,y,z;
+    gyro.getXYZ(&x,&y,&z);
+    float wx,wy,wz;
+    char m[6];
+    gyro.getAngularVelocity(&wx,&wy,&wz);
+    anglex=wx*0.03;
+    angley=wy*0.03;
+    anglez=wy*0.03;
+    //anglex=abs(anglex);
+    //degx= (degx+anglex);
+    degx = (wx/2000)*(degx + anglex)+(1-(wx/2000))*tilt;
+    output = movingAverageFilter.process(degx);
+    gz_prev = z;
+    out1=abs(H-output);
+    out2=H-output;
+    out2= abs(out2);
+    if (out2>140) {out2=140;}
+    sprintf(m,"A#%d#B",out2);
+  char *p;
+  p = m;
+  /*for (p=m; *p != '\0'; p++)
+  {
+    //Serial.write('\n');
+   SerialBT.print(*p);
+   Serial.print(*p);
+   delay (50);  
+  }
+  */  
+  SerialBT.print(p);
+  Serial.println(out2);
+
+  delay (30);
+  // Chill
+}
